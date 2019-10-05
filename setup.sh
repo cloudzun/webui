9b266dd1b6a613cbf9b2492843ff2b6728470cfa
CloudZun <info@cloudzun.com>
2019-10-05 20:27:57 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+az group create --name webui --location southeastasia
+az vm create --resource-group webui --name mywebui01 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B1s --custom-data SS.txt  
+az vm open-port --resource-group webui --name mywebui01 --port 6800-6888 --priority 1001
